4bf714cbb7e6eee656a0e7e52658811fd94826d7
Kevin <kevin.raszka@tria-tr.cz>
2026-04-29 12:49:15 +0200

    kviz

---
+++ kvíz_na_vesmír/kvíz.py
@@ -0,0 +1,13 @@
+def kviz_vesmir():
+    otazky = [
+        {
+            "otazka": "Jaká je největší planeta ve sluneční soustavě?",
+            "moznosti": ["A) Země", "B) Jupiter", "C) Mars", "D) Saturn"],
+            "odpoved": "B"
+        },
+        {
+            "otazka": "Která galaxie je nejblíže Mléčné dráze?",
+            "moznosti": ["A) Andromeda", "B) Sombrero", "C) Trojúhelník", "D) Vírová"],
+            "odpoved": "A"
+        }
+    ]
